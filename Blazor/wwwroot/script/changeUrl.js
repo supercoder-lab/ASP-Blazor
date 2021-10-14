b2ded59b833398022b6d39e4ca9e85037d6100fd
BlockChainDev-a <7.bremi.2@gmail.com>
2021-10-14 06:31:26 +0100

    Model, UI completed

---
+++ Blazor/wwwroot/script/changeUrl.js
@@ -0,0 +1,39 @@
+﻿
+window.changeImgUrlFunctions = {
+    section1ImgChangeValid: function () {
+        document.getElementById("section1Img").src = "./img/check_valid.png";
+    },
+    section1ImgChangeInvalid: function () {
+        document.getElementById("section1Img").src = "./img/check_invalid.png";
+    },
+    section2ImgChangeValid: function () {
+        document.getElementById("section2Img").src = "./img/check_valid.png";
+    },
+    section2ImgChangeInvalid: function () {
+        document.getElementById("section2Img").src = "./img/check_invalid.png";
+    },
+    section3ImgChangeValid: function () {
+        document.getElementById("section3Img").src = "./img/check_valid.png";
+    },
+    section3ImgChangeInvalid: function () {
+        document.getElementById("section3Img").src = "./img/check_invalid.png";
+    },
+    section1StatusValid: function () {
+        document.getElementById("section1Status").src = "./img/check_valid.png";
+    },
+    section1StatusInValid: function () {
+        document.getElementById("section1Status").src = "./img/check_invalid.png";
+    },
+    section2StatusValid: function () {
+        document.getElementById("section2Status").src = "./img/check_valid.png";
+    },
+    section2StatusInValid: function () {
+        document.getElementById("section2Status").src = "./img/check_invalid.png";
+    },
+    section3StatusValid: function () {
+        document.getElementById("section3Status").src = "./img/check_valid.png";
+    },
+    section3StatusInValid: function () {
+        document.getElementById("section3Status").src = "./img/check_invalid.png";
+    }
+}
